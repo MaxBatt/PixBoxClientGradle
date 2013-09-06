9c2159bf35b9ba04fc12087b04f3dda607ebbf9e
Max Batt <mail@maxbatt.de>
2013-09-06 21:43:42 +0200

    Changed Webcache for ImgViews

--- PixBoxAndroidClient/src/main/java/de/pixbox/client/helpers/GalleryListAdapter.java
+++ PixBoxAndroidClient/src/main/java/de/pixbox/client/helpers/GalleryListAdapter.java
@@ -3,6 +3,8 @@ package de.pixbox.client.helpers;
 
 import java.util.ArrayList;
 
+import com.loopj.android.image.SmartImageView;
+
 import de.pixbox.client.R;
 
 import android.content.Context;
@@ -10,7 +12,6 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WebCachedImageView;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
@@ -20,47 +21,47 @@ import android.widget.TextView;
  * Every single row of the PixBox listView is built here.</br>
  * Thumbnails of the online images are downloaded and put into an imageView.</br>
  * This is done in a cached way by a WebCachedImageView (see https://github.com/leocadiotine/WebCachedImageView)
- * 
- *  @author Max Batt
+ *
+ * @author Max Batt
  */
 public class GalleryListAdapter extends ArrayAdapter<Image> {
-	private final Context context;
-	private final ArrayList<Image> imageList;
+    private final Context context;
+    private final ArrayList<Image> imageList;
 
-	private WebCachedImageView imgView;
-	private TextView tvUploadInfo;
-	
+    private SmartImageView imgView;
+    private TextView tvUploadInfo;
 
-	// Build ListAdapter from ArrayList
-	public GalleryListAdapter(Context context,
-			ArrayList<Image> imageList) {
-		super(context, R.layout.image_row, imageList);
-		this.context = context;
-		this.imageList = imageList;
-	}
 
-	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
+    // Build ListAdapter from ArrayList
+    public GalleryListAdapter(Context context,
+                              ArrayList<Image> imageList) {
+        super(context, R.layout.image_row, imageList);
+        this.context = context;
+        this.imageList = imageList;
+    }
 
-		// Fill ListRow
-		LayoutInflater inflater = (LayoutInflater) context
-				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		View rowView = inflater.inflate(R.layout.image_row, parent, false);
-				
-		
-		// Put image in row via WebCachedImageView
-		imgView = (WebCachedImageView) rowView.findViewById(R.id.imgView);
-		imgView.setImageUrl(imageList.get(position).getThumbURL());
-		
-		tvUploadInfo = (TextView) rowView.findViewById(R.id.tvUploadInfo);
-		
-		System.out.println("img id: " + imageList.get(position).getId());
-		tvUploadInfo.setText(imageList.get(position).getCreatedAt());
-		
-		
-		// Return ListRow.
-		return rowView;
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
 
-	}
+// Fill ListRow
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowView = inflater.inflate(R.layout.image_row, parent, false);
+
+
+// Put image in row via WebCachedImageView
+        imgView = (SmartImageView) rowView.findViewById(R.id.imgView);
+        imgView.setImageUrl(imageList.get(position).getThumbURL());
+
+        tvUploadInfo = (TextView) rowView.findViewById(R.id.tvUploadInfo);
+
+        System.out.println("img id: " + imageList.get(position).getId());
+        tvUploadInfo.setText(imageList.get(position).getCreatedAt());
+
+
+// Return ListRow.
+        return rowView;
+
+    }
 
 }
